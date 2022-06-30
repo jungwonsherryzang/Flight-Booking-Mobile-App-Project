879be9ab4b2286cf6e823041d13cbae9502dc986
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-30 01:40:36 -0400

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -1,48 +1,57 @@
-import { View, Text } from 'react-native';
+import { TouchableOpacity, View, Text, Image } from 'react-native';
 import React from 'react';
 
-import { TouchableOpacity, View, Text } from 'react-native';
-//TouchableOpacity: just touching 
+import { COLORS } from '../constants/theme.js';
+import { SIZES } from '../constants/theme.js';
+import { SHADOWS } from '../constants/theme.js';
+import { FONTS } from '../constants/theme.js';
 
-import { COLORS, SIZES, FONTS, SHADOWS } from '../constants';
-
-
-
-export const CircleButton = ({ imgUrl, ...props }) => {
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => { 
   return (
-    <TouchableOpacity
-        style={{
-            width: 40,
-            height: 40,
-            backgroundColor: COLORS.white,
-            position: 'absolute',
-            borderRadius: SIZES.extraLarge,
-            alignItems: 'center',
-            justifyContent: 'center',
-            ...SHADOWS.light,
-            ...props,
-        }}
-    >
-
-    //하트버튼
-    <Image 
-        source={imgUrl}
-        resizeMode="contain"
-        style={{ width: 24, height: 24 }}
-    />
-
-
-        
+    <TouchableOpacity 
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white, 
+        position: 'absolute', //누를수있는 네모난 버튼 왼쪽 위에 생기고
+        borderRadius: SIZES.extraLarge, //버튼 모양 동그랗게
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props //props 위에 설정하고 여기까지하면 오른쪽 위에 동그라미 생김
+      }}
+      onPress={handlePress}
+      >
+        <Image 
+          source={imgUrl} //FlightCard.js에 설정해노음
+          resizeMode="contain"
+          style={{ width: 24, height: 24 }}
+        />
 
     </TouchableOpacity>
   )
 }
 
-export const RectButton = () => {
+export const RectButton = ({ handlePress, fontSize, minWidth, ...props }) => {
     return (
-      <View>
-        <Text>Button</Text>
-      </View>
+      <TouchableOpacity 
+        style={{
+          backgroundColor: COLORS.indianred, 
+          borderRadius: SIZES.extraLarge, //버튼 모양 동그랗게
+          minWidth: minWidth,
+          padding: SIZES.small,
+          ...props //props 위에 설정하고 여기까지하면 오른쪽 위에 동그라미 생김
+        }}
+        onPress={handlePress}
+        >
+          <Text style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: fontSize,
+            color: COLORS.white,
+            textAlign: 'center',
+          }}>
+          Book Flight</Text>
+      </TouchableOpacity>
     )
-  }
+}
 
