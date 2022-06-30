f93459cb57ed70dc8720266e9cf84b9b55396b8b
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-30 01:44:29 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './screens/Home';
+import Home from './screens/Home.js';
+import Details from './screens/Details.js';
 
 const Stack = createStackNavigator();
 
 const theme = {
     ...DefaultTheme,
+
     colors: {
         ...DefaultTheme.colors,
-        background: "transparent"
+        background: "transparent",
     }
 }
 
+
+
 const App = () => {
 
-    const [loaded] = useFonts({
+    //initializing the fonts throughout entire application
+    const [used] = useFonts({
         InterBold: require("./assets/fonts/Inter-Bold.ttf"),
         InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
         InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
@@ -24,15 +29,17 @@ const App = () => {
         InterLight: require("./assets/fonts/Inter-Light.ttf"),
     });
 
-    if (!loaded) return null;
+    if (!used) return null;
 
 
-    return (
-        <NavigationContainer theme = {theme}>
-            <Stack.Navigator screenOptions = {{ headerShow: false }}
-            initialRouteName="Home">
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer theme={theme}>
+        <Stack.Navigator screenOptions={{ headerShow: false }} initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} /> 
+            <Stack.Screen name="Details" component={Details} /> 
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
