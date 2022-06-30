caaf9fde3e63995f43b4e19433831bed1b889bfa
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-30 01:42:39 -0400

    Add files via upload

---
+++ constants/assets.js
@@ -0,0 +1,33 @@
+import heart from "../assets/icons/heart.png";
+import dollar from "../assets/icons/dollar.png";
+import search from "../assets/icons/search.png";
+import users from "../assets/images/users.png";
+
+import logo from "../assets/images/logo.png";
+import flight1 from "../assets/images/flight1.png";
+import flight2 from "../assets/images/flight2.jpeg";
+import flight3 from "../assets/images/flight3.jpeg";
+import flight4 from "../assets/images/flight4.jpeg";
+import flight5 from "../assets/images/flight5.jpeg";
+import flight6 from "../assets/images/flight6.jpeg";
+import flight7 from "../assets/images/flight7.jpg";
+
+
+
+export default {
+  dollar,
+  heart,
+  search,
+  users,
+
+  logo,
+  flight1,
+  flight2,
+  flight3,
+  flight4,
+  flight5,
+  flight6,
+  flight7,
+
+
+};
