6317685cdc95a91ee8433befd1b4892f4bc6cf48
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-29 20:00:49 -0400

    Update and rename StatusBar.js to TopStatusBar.js

--- components/TopStatusBar.js
+++ components/TopStatusBar.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'react-native';
 import React from 'react';
 
-const StatusBar = () => {
+const TopStatusBar = () => {
   const useIsFocused = useIsFocused();
 
 
   return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
 
-export default StatusBar;
+export default TopStatusBar;
