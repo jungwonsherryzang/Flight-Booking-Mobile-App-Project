f932b68ab5d7f570724a560c53ba207871de0e4a
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-30 01:46:38 -0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { View, SafeAreaView, FlatList, Text } from 'react-native'; 
-//flatlist is efficient than scrollview
-
+import { View, Text, SafeAreaView, FlatList } from 'react-native';
+//flatlist is more efficient than scrollview
+import React, { useState } from 'react';
 import { COLORS } from '../constants/theme.js';
 import { FlightData } from '../constants/data.js';
 
@@ -11,39 +10,36 @@ import { TopStatusBar } from '../components/index.js';
 import { FlightCard } from '../components/index.js';
 
 
+
 const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-        //status bar 위에 검은띠
-        <StatusBar background={COLORS.primary} /> 
+      <TopStatusBar background={COLORS.primary} />
 
-        <View style={{ flex: 1}}>
-            <View style={{ zIndex: 0 }}>
-            <FlatList 
-                data={FlightData}  //Later, this data will be changed to receive real-time data through API
-                renderItem={({ item }) => <FlightCard data={item} />}
-                showsVerticalScrollIndicator={false}
-                keyExtractor={(item) => item.id}
-                ListHeaderComponent={<HomeHeader />}
-            />
-            </View>
-
-            <View style={{
-                position: "absolute",
-                top: 0,
-                bottom: 0,
-                right: 0,
-                left: 0,
-                zIndex: -1,
-            }}>
-
-            <View style={{ height: 300, background: COLORS.primary }} />
-            <View style={{ flex: 1, background: COLORS.white }} />
-
-            </View>
-
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList 
+            data={FlightData}
+            renderItem={({ item }) => <FlightCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
         </View>
-        
+
+        <View style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          zIndex: -1,
+        }}>
+          <View style={{ height: 300, backgroundColor: COLORS.lightpink}} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white}} />
+        </View>
+      </View>
+      
     </SafeAreaView>
   )
 }
