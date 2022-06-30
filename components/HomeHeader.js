c0e3c2159144256413d7792bde5e2607551c28bc
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-30 01:48:52 -0400

    Create HomeHeader.js

---
+++ components/HomeHeader.js
@@ -0,0 +1,14 @@
+//navigation bar
+//giving an info about the current logged in person
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeHeader = () => {
+  return (
+    <View>
+      <Text>HomeHeader</Text>
+    </View>
+  )
+}
+
+export default HomeHeader
