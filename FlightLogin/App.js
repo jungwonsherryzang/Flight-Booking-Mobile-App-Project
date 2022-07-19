c234c4ef8ea1e6da2e8c4ff7cfebbae34e2871d9
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-07-19 10:58:12 -0400

    Create App.js

---
+++ FlightLogin/App.js
@@ -0,0 +1,45 @@
+import { useFonts } from 'expo-font';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home.js';
+import Details from './screens/Details.js';
+
+const Stack = createStackNavigator();
+
+const theme = {
+    ...DefaultTheme,
+
+    colors: {
+        ...DefaultTheme.colors,
+        background: "transparent",
+    }
+}
+
+
+
+const App = () => {
+
+    //initializing the fonts throughout entire application
+    const [used] = useFonts({
+        InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+        InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+        InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+        InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+        InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    });
+
+    if (!used) return null;
+
+
+  return (
+    <NavigationContainer theme={theme}>
+        <Stack.Navigator screenOptions={{ headerShow: false }} initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} /> 
+            <Stack.Screen name="Details" component={Details} /> 
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
